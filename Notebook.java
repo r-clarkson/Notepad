0616a47a958e7085827b586538775554d5bc03f2
riley <rclarkson@luc.edu>
2017-09-17 16:42:49 -0500

    Added comments for notebook class

--- Notebook.java
+++ Notebook.java
@@ -1,7 +1,10 @@
 import java.util.HashMap<K,V>;
 import java.util.LinkedList
 import java.util.Map<E>;
+/**
 
+IGNORE everything in here we're working on replanning this class
+**/
 public class Notebook{
 	Map<String, LinkedList<String>> topicMention = HashMap<String, LinkedList<String>>;
 	LinkedList<String> topicMentionLL = new LinkedList<String>();
@@ -9,11 +12,11 @@ public class Notebook{
 	LinkedList<String> uniqueMentionLL = new LinkedList<String>();
 	LinkedList<String> refeferenceMentionLL = new LinkedList<String>();
 	LinkedList<String> urlLL = new LinkedList<String>();
-	
+
 	public void passNote(Note note){
-		
+
 	}
-	
+
 	public void intializeMap(List<String> list){
 		for (int i = 0; i < list.size(); i++){
 			topicMention.put(n.getTopicMention().get(i), topicMentionLL);
