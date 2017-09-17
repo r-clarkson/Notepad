c8963de31e6886facff0381d51d70d5db18aec32
riley <rclarkson@luc.edu>
2017-09-17 12:15:14 -0500

    Started parsing function

--- Note.java
+++ Note.java
@@ -1,6 +1,6 @@
 import java.io.File;
 import java.util.*;
-import java.util.regex.Pattern;
+import java.util.regex.*;
 import java.nio.file.Files;
 import java.nio.charset.StandardCharsets;
 import java.io.IOException;
@@ -17,26 +17,43 @@ public class Note{
 
   }
   /** function will turn files text line by line into strings, create patterns for each type of identifier and match with these strings
-  then the matches will be stored in a list**/
+  then the matches will be stored in a list
+  https://stackoverflow.com/questions/326390/how-do-i-create-a-java-string-from-the-contents-of-a-file
+  https://stackoverflow.com/questions/10432543/extract-hash-tag-from-string
+  **/
   public void parseNote(File note){
+    //declare patterns here for each type of identifier
+    Pattern topicMention = Pattern.compile("#[-a-zA-Z0-9_]+");
+    //make a list for each type
+    List<String> topicMentionList = new ArrayList<String>();
 
     try{
       //turns each line from the file into a string, given the character encoding type and filepath
       List<String> lines = Files.readAllLines(note.toPath(), StandardCharsets.ISO_8859_1);
-      //for now prints each line but eventually we put in the pattern stuff in here
-      for (int i = 0; i < lines.size(); i ++){
+      //declare a matcher to use to search for the patterns
+      Matcher m;
 
-        System.out.println(lines.get(i));
+      for (int i = 0; i < lines.size(); i ++){
+        //look through each line and add any matches to the proper arraylist
+        m = topicMention.matcher(lines.get(i));
+
+        while(m.find()){
+          topicMentionList.add(m.group());
+        }
 
       }
+      //test function for now, prints out the results so we can check them...
+      for (int i =0; i < topicMentionList.size(); i++){
 
+        System.out.println(topicMentionList.get(i));
+
+      }
     }
     catch (IOException e){
       //prints stack trace if there's an exception
       e.printStackTrace();
 
     }
-
 
   }
 
