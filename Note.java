36f2b3300b34348a52709a652b0495dc33d3191a
Joseph <jconroy5@luc.edu>
2017-09-17 15:46:30 -0500

    note class in progress

--- Note.java
+++ Note.java
@@ -8,9 +8,21 @@ import java.io.IOException;
 public class Note{
 
   String name;
+  public List<String> topicMentionList;
+  List<String> individualMentionList;
+  List<String> uniqueMentionList;
+  List<String> referenceMentionList;
+  List<String> urlList;
 
   /** constructor will set filename to identify file by and then pass the file to be parsed in a separate function**/
   public Note(File note){
+
+    //make a list for each type
+    topicMentionList = new ArrayList<String>();
+    individualMentionList = new ArrayList<String>();
+    uniqueMentionList = new ArrayList<String>();
+    referenceMentionList = new ArrayList<String>();
+    urlList = new ArrayList<String>();
 
     name = note.getName();
     parseNote(note);
@@ -18,37 +30,34 @@ public class Note{
   }
   /** function will turn files text line by line into strings, create patterns for each type of identifier and match with these strings
   then the matches will be stored in a list
+  passes pattern, list for type of identifier, and the list of lines to another parsing function which goes through for each type of pattern
   https://stackoverflow.com/questions/326390/how-do-i-create-a-java-string-from-the-contents-of-a-file
   https://stackoverflow.com/questions/10432543/extract-hash-tag-from-string
   **/
   public void parseNote(File note){
     //declare patterns here for each type of identifier
     Pattern topicMention = Pattern.compile("#[-a-zA-Z0-9_]+");
+<<<<<<< HEAD
     Pattern topicMention = Pattern.compile("@[-a-zA-Z0-9_]+");
     //make a list for each type
     List<String> topicMentionList = new ArrayList<String>();
+=======
+    Pattern individualMention = Pattern.compile("@[-a-zA-Z0-9_]+");
+    Pattern uniqueMention = Pattern.compile("![-a-zA-Z0-9_]+");
+    Pattern referenceMention = Pattern.compile("\\^[-a-zA-Z0-9_]+");
+    //Pattern url = Pattern.compile("((http:\\/\\/|https:\\/\\/)?(www.)?([a-zA-Z0-9]+).[a-z]{3})");
+>>>>>>> branch 'master' of https://github.com/r-clarkson/Notepad.git
 
     try{
       //turns each line from the file into a string, given the character encoding type and filepath
       List<String> lines = Files.readAllLines(note.toPath(), StandardCharsets.ISO_8859_1);
-      //declare a matcher to use to search for the patterns
-      Matcher m;
 
-      for (int i = 0; i < lines.size(); i ++){
-        //look through each line and add any matches to the proper arraylist
-        m = topicMention.matcher(lines.get(i));
+      setTopicMention(identifierMatcher(lines,topicMentionList,topicMention));
+      setIndividualMention(identifierMatcher(lines,individualMentionList,individualMention));
+      setUniqueMention(identifierMatcher(lines,uniqueMentionList,uniqueMention));
+      setReferenceMention(identifierMatcher(lines,referenceMentionList,referenceMention));
+      //identifierMatcher(lines,urlList,url);
 
-        while(m.find()){
-          topicMentionList.add(m.group());
-        }
-
-      }
-      //test function for now, prints out the results so we can check them...
-      for (int i =0; i < topicMentionList.size(); i++){
-
-        System.out.println(topicMentionList.get(i));
-
-      }
     }
     catch (IOException e){
       //prints stack trace if there's an exception
@@ -57,8 +66,70 @@ public class Note{
     }
 
   }
+  /**
+  Given the list of lines, identifier list, and pattern, matches for the size of the note. Then adds matches to the list of the identifier.
+  **/
+  public List<String> identifierMatcher(List<String> lines, List<String> identifierList, Pattern p){
+
+
+    for (int i = 0; i < lines.size(); i++){
+
+      Matcher match = p.matcher(lines.get(i));
+
+      while(match.find()){
+
+        identifierList.add(match.group());
+
+      }
+    }
+
+    return identifierList;
+  }
 
   public String getName(){
+
     return name;
+
+  }
+  public List<String> getTopicMention(){
+    return topicMentionList;
+  }
+  public List<String> getIndividualMention(){
+    return individualMentionList;
+  }
+  public List<String> getUniqueMention(){
+    return uniqueMentionList;
+  }
+  public List<String> getReferenceMention(){
+    return referenceMentionList;
+  }
+  public List<String> getURL(){
+    return urlList;
+  }
+
+  public void setTopicMention(List<String> topicMentionList){
+
+    topicMentionList = this.topicMentionList;
+
+  }
+  public void setIndividualMention(List<String> individualMentionList){
+
+    individualMentionList = this.individualMentionList;
+
+  }
+  public void setUniqueMention(List<String> uniqueMentionList){
+
+    uniqueMentionList = this.uniqueMentionList;
+
+  }
+  public void setReferenceMention(List<String> referenceMentionList){
+
+    referenceMentionList = this.referenceMentionList;
+
+  }
+  public void setURL(List<String> urlMentionList){
+
+    urlList = this.urlList;
+
   }
 }
