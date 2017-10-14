4b1ad9af8fc2229a7c4e2dfb3f4db9d0dadd69e9
riley <rclarkson@luc.edu>
2017-10-14 17:04:52 -0500

    Added new test class

---
+++ src/test/java/testNotebookManager.java
@@ -0,0 +1,13 @@
+package notepad.src.main.java.code;
+import junit.framework.*;
+import static org.junit.Assert.*;
+import java.io.*;
+import java.nio.file.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class testNotebookManager{
+
+
+}
