51a3ab80ef5b8f7bf0487062a1e19a37cc5ab8fe
riley <rclarkson@luc.edu>
2017-10-15 16:20:26 -0500

    Edit note function working

--- src/main/java/code/NotebookManager.java
+++ src/main/java/code/NotebookManager.java
@@ -18,14 +18,22 @@ public class NotebookManager{
   Scanner scan = new Scanner(System.in);
   /** given the user's input, adds a note of that type */
   public void editNote(String type){
-    if (type.equals("-t")){
-      addTypedNote(getNoteFile());
-    }
-    else if (type.equals("-d")){
-      addDictatedNote(getNoteFile());
-    }
-    else if (type.equals("-i")){
-      addIdentifier(getNoteFile());
+    switch (type){
+      case "-t":
+        addTypedNote(getNoteFile());
+        break;
+      case "-d":
+        addDictatedNote(getNoteFile());
+        break;
+      case "-i":
+        addIdentifier(getNoteFile());
+        break;
+      case "-e":
+        appendToNote(getNoteFile());
+        break;
+      default:
+        System.out.println("Option not recognized.");
+        break;
     }
   }
 
@@ -50,6 +58,23 @@ public class NotebookManager{
     }
     return newNote;
   }
+  /**
+  * Adds text to an already existing file
+  **/
+  public void appendToNote(File filename){
+    System.out.println("Please type what you would like to append to " + filename.getName() + ". Skip a line and press enter to submit changes");
+    String text = scan.nextLine();
+    while (!text.equals("")){
+      try{
+        Files.write(Paths.get(filename.getPath()), text.getBytes(), StandardOpenOption.APPEND);
+      }
+      catch (IOException e){
+        System.out.println("Error editing file.");
+      }
+      text = scan.nextLine();
+    }
+    System.out.println(filename.getName() + " edited.");
+  }
 
   /**
   * TODO: Method will use getNoteFile to get the text file to be written to.
@@ -63,12 +88,12 @@ public class NotebookManager{
       PrintWriter writer = new PrintWriter(filename);
       System.out.println("Enter the body of your note. When you are finished please type EOF: \n ");
       do {
-          body = scan.next();
-          writer.write(body);
-        }while(!(scan.next().equals("EOF")));
-        writer.close();
-        System.out.println("File written properly!");
-      } catch (IOException e) {
+        body = scan.next();
+        writer.write(body);
+      }while(!(scan.next().equals("EOF")));
+      writer.close();
+      System.out.println("File written properly!");
+    } catch (IOException e) {
       System.out.println("File did not write properly");
     }
     newNote = new Note(filename);
@@ -77,7 +102,7 @@ public class NotebookManager{
     } else {
       System.out.println("Note Object Not Created");
     }
-  return newNote;
+    return newNote;
   }
 
   /**
@@ -85,96 +110,97 @@ public class NotebookManager{
   * if the LL of a certain key in the HM is only size 1, then delete the key because it only existed due to that file
 
   public void deleteNote(Notebook nb, File filename){
-    LinkedList<String>noteList=nb.getNotesList();
-    for(int i;len(noteList);i++){
+  LinkedList<String>noteList=nb.getNotesList();
+  for(int i;len(noteList);i++){
 
-    }
-  }
+}
+}
 */
 
-  /**
-  * Using Sphinx4 API, record from user's microphone and then add recording to a text file
-  * See inside for more details
-  */
-  public Note addDictatedNote(File filename){
-    /** set up a configuration for english, and set recording variable to true */
-    Configuration configuration = new Configuration();
-    boolean record = true;
-    configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
-    configuration.setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
-    configuration.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
+/**
+* Using Sphinx4 API, record from user's microphone and then add recording to a text file
+* See inside for more details
+*/
+public Note addDictatedNote(File filename){
+  /** set up a configuration for english, and set recording variable to true */
+  Configuration configuration = new Configuration();
+  boolean record = true;
+  configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
+  configuration.setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
+  configuration.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
 
-    try {
-      /** clear the screen and give user instructions */
-      System.out.print("\033[H\033[2J");
-      System.out.flush();
-      System.out.println("Dependencies loaded. Dictate your note and type 'stop' to stop.");
-      /* somehwat messy fix to keep log messages from sphinx4 appearing in the console https://stackoverflow.com/questions/35560969/disable-console-mess-in-cmusphinx4 */
-      Logger cmRootLogger = Logger.getLogger("default.config");
-      cmRootLogger.setLevel(java.util.logging.Level.OFF);
-      String conFile = System.getProperty("java.util.logging.config.file");
-      if (conFile == null) {
-        System.setProperty("java.util.logging.config.file", "ignoreAllSphinx4LoggingOutput");
-      }
-      ArrayList<SpeechResult> results = new ArrayList<SpeechResult>();
-      /** initialize recognizer which uses the microphone to start speech recognition */
-      LiveSpeechRecognizer recognizer = new LiveSpeechRecognizer(configuration);
-      recognizer.startRecognition(true);
-      /** result from recognizer is taken and the hypothesis of the word is written to the file */
-      SpeechResult result = recognizer.getResult();
-      results.add(result);
-      //TODO: there should be a better flow here/better way to keep recording going and then stop on command
-      if (scan.next()=="stop"){
-        recognizer.stopRecognition();
-      }
+  try {
+    /** clear the screen and give user instructions */
+    System.out.print("\033[H\033[2J");
+    System.out.flush();
+    System.out.println("Dependencies loaded. Dictate your note and type 'stop' to stop.");
+    /* somehwat messy fix to keep log messages from sphinx4 appearing in the console https://stackoverflow.com/questions/35560969/disable-console-mess-in-cmusphinx4 */
+    Logger cmRootLogger = Logger.getLogger("default.config");
+    cmRootLogger.setLevel(java.util.logging.Level.OFF);
+    String conFile = System.getProperty("java.util.logging.config.file");
+    if (conFile == null) {
+      System.setProperty("java.util.logging.config.file", "ignoreAllSphinx4LoggingOutput");
+    }
+    ArrayList<SpeechResult> results = new ArrayList<SpeechResult>();
+    /** initialize recognizer which uses the microphone to start speech recognition */
+    LiveSpeechRecognizer recognizer = new LiveSpeechRecognizer(configuration);
+    recognizer.startRecognition(true);
+    /** result from recognizer is taken and the hypothesis of the word is written to the file */
+    SpeechResult result = recognizer.getResult();
+    results.add(result);
+    //TODO: there should be a better flow here/better way to keep recording going and then stop on command
+    if (scan.next()=="stop"){
+      recognizer.stopRecognition();
+    }
 
-      for (int i = 0; i < results.size(); i++){
-        Files.write(Paths.get(filename.getPath()), results.get(i).getHypothesis().getBytes(), StandardOpenOption.APPEND);
-      }
-      /** recording is stopped when user types the command */
+    for (int i = 0; i < results.size(); i++){
+      Files.write(Paths.get(filename.getPath()), results.get(i).getHypothesis().getBytes(), StandardOpenOption.APPEND);
     }
-    catch (IOException e){
-      //TODO: better options here. user should be able to quit or go back to main menu or something.
-      System.out.println("Error.");
-    }
-    /** create new note from the text file which was added to and then return it */
-    Note newNote = new Note(filename);
-    return newNote;
+    /** recording is stopped when user types the command */
   }
+  catch (IOException e){
+    //TODO: better options here. user should be able to quit or go back to main menu or something.
+    System.out.println("Error.");
+  }
+  /** create new note from the text file which was added to and then return it */
+  Note newNote = new Note(filename);
+  return newNote;
+}
 
-  /**
-  * Method to create and return new text file if user is adding a file
-  * Otherwise will return the filename the user wishes to edit
-  */
-  public File getNoteFile(){
+/**
+* Method to create and return new text file if user is adding a file
+* Otherwise will return the filename the user wishes to edit
+*/
+public File getNoteFile(){
 
-    File noteFile = new File(".." + File.separator + "Notepad" + File.separator + "notes");
-    System.out.println("Please enter the name of the note you would like to edit to or create (do not include file type, only the name). For a list of current note filenames, please type 'list'.");
-    String input = scan.nextLine();
-    /** lists files available to edit or creates filename user typed in */
-    try{
-      if (input.equals("list")){
-        if (noteFile.isDirectory()){
-          for (final File fileEntry : noteFile.listFiles()) {
-            System.out.println("File: " + fileEntry.getName());
-          }
+  File noteFile = new File(".." + File.separator + "Notepad" + File.separator + "notes");
+  System.out.println("Please enter the name of the note you would like to edit to or create (do not include file type, only the name). For a list of current note filenames, please type 'list'.");
+  String input = scan.nextLine();
+  /** lists files available to edit or creates filename user typed in */
+  try{
+    if (input.equals("list")){
+      if (noteFile.isDirectory()){
+        for (final File fileEntry : noteFile.listFiles()) {
+          System.out.println("File: " + fileEntry.getName());
         }
-        getNoteFile();
       }
-      // TODO: only functionality for creating a new note at the moment. need to be able to edit existing notes as well.
-      else{
-        //create new text file (in notes folder) with filename entered and return it
-        noteFile = new File(".." + File.separator + "Notepad" + File.separator + "notes" + File.separator + input + ".txt");
+      getNoteFile();
+    }
+    else {
+      //create new text file (in notes folder) with filename entered and return it
+      noteFile = new File(".." + File.separator + "Notepad" + File.separator + "notes" + File.separator + input + ".txt");
+      if (!noteFile.exists()){
         Files.createFile(Paths.get(noteFile.getPath()));
-        System.out.println(noteFile.getName() + " created in notes directory.");
       }
+      System.out.println(noteFile.getName() + " retrieved");
     }
-    catch (NullPointerException e){
-      System.out.println("Null Pointer Error.");
-    }
-    catch (IOException i){
-      System.out.println("IO Error.");
-    }
-    return noteFile;
   }
+  catch (NullPointerException e){
+    System.out.println("Null Pointer Error.");
+  }
+  catch (IOException i){
+    System.out.println("IO Error.");
+  }
+  return noteFile;
+}
 }
